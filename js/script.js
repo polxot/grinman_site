d1e0c82a7afd883de8f46a4a8dd8eb76ef886e7b
Pavel Tamarin <tampol40@gmail.com>
2024-05-14 10:08:15 +0300

    new_grinman

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('#menu');
+let openMenu = document.querySelector('header');
+
+
+menu.onclick = () => {
+	menu.classList.toggle('fa-times');
+	openMenu.classList.toggle('active');
+}
